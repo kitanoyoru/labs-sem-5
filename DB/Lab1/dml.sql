a8e449d0aaee0d1e5617d389b0c335baaee18b30
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-09-08 15:34:41 +0300

    feat(db): dml

--- DB/Lab1/dml.sql
+++ DB/Lab1/dml.sql
@@ -65,8 +65,8 @@ FROM "Количество_Деталей" AS a
 WHERE d."Город" IN (SELECT MIN(c."Город")
                     FROM "Проекты_j" AS c);
 
--- 17 --
-SELECT a."Д", a."ПР", SUM(CAST(a.s AS INTEGER))
+-- 18 --
+SELECT a."Д", a."ПР",  AVG(CAST(a.s AS INTEGER))
 FROM "Количество_Деталей" AS a
 GROUP BY a."Д", a."ПР";
 
@@ -95,19 +95,22 @@ WHERE a."П" = 'П1'
 SELECT a."Д"
 FROM "Количество_Деталей" AS a
          INNER JOIN "Проекты_j" AS b ON a."ПР" = b."ПР"
-    AND b."Город" = 'Минск';
+    AND b."Город" = 'Псков';
 
 -- 3 --
 SELECT a."П"
 FROM "Количество_Деталей" AS a
 WHERE a."ПР" = 'ПР1';
 
--- 8 -- not solved
+-- 8 --
 SELECT DISTINCT a."П", a."Д", a."ПР"
 FROM "Количество_Деталей" AS a
          INNER JOIN "Поставщики_s" AS b ON a."П" = b."П"
          INNER JOIN "Детали_p" AS c ON a."Д" = c."Д"
-         INNER JOIN "Проекты_j" AS d ON a."ПР" = d."ПР";
+         INNER JOIN "Проекты_j" AS d ON a."ПР" = d."ПР"
+WHERE b."Город" <> c."Город"
+  AND c."Город" <> d."Город"
+  AND b."Город" <> d."Город";
 
 -- 12 --
 SELECT a."Д"
@@ -121,28 +124,38 @@ SELECT SUM(CAST(a.s AS INTEGER))
 FROM "Количество_Деталей" AS a
 WHERE a."П" = 'П1';
 
--- 32 --
+-- 32 -- !
 SELECT b."ПР"
 FROM "Количество_Деталей" AS b
 WHERE b."Д" IN (SELECT a."Д"
                 FROM "Количество_Деталей" AS a
-                WHERE a."П" = 'П1');
+                WHERE a."П" = 'П2');
 
--- 26 --
+-- 26 -- !
 SELECT b."ПР"
 FROM "Количество_Деталей" AS b,
      (SELECT a."ПР", AVG(CAST(a.s AS INTEGER)) AS average
       FROM "Количество_Деталей" AS a
-      WHERE a."Д" = 'Д1'
+      WHERE a."Д" = 'Д3'
       GROUP BY a."ПР") AS q1,
      (SELECT MAX(CAST(a.s AS INTEGER)) AS maximum
       FROM "Количество_Деталей" AS a
       WHERE a."ПР" = 'ПР1') AS q2
 WHERE q1.average > q2.maximum;
 
--- 18 --
-SELECT a."Д", a."ПР",  AVG(CAST(a.s AS INTEGER))
+-- 17 -- !
+SELECT a."Д", a."ПР", SUM(CAST(a.s AS INTEGER))
 FROM "Количество_Деталей" AS a
 GROUP BY a."Д", a."ПР";
+
+-- Получить фамилии преподавателей, преподающих те же предметы, что и преподаватель
+-- преподающий предмет с номером 14П.
+
+
+
+
+
+
+
 
 
