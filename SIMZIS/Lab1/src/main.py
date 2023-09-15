4987cc937420a099cb89d942a36903454ec46bf5
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-09-15 21:27:20 +0300

    feat(omis): lab1

--- SIMZIS/Lab1/src/main.py
+++ SIMZIS/Lab1/src/main.py
@@ -3,20 +3,17 @@ import itertools
 
 from contants import ALPHABET
 
-
 def generate_random_password(length: int) -> str:
     generated_string = "".join(random.choice(ALPHABET) for _ in range(length))
     return generated_string
 
 
-# @log_time_decorator
 def password_selection(target_password: str):
     for length in range(1, len(ALPHABET) + 1):
         for guess in itertools.product(ALPHABET, repeat=length):
             guessed_password = "".join(guess)
             if guessed_password == target_password:
                 return
-
 
 if __name__ == "__main__":
     password_length = int(input("Введите длину строки: "))
