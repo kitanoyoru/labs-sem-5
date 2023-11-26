6e709450dbcbb14806115a7a5b27fe681957d6dd
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-11-26 18:35:17 +0300

    Dev

--- AIPOS/Lab2/docker/docker-entrypoint.sh
+++ AIPOS/Lab2/docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # activate our virtual environment here
 . /opt/pysetup/.venv/bin/activate
 
-# You can put other setup logic here
+python3 -m src reset-db
 
 # Evaluating passed command:
 exec "$@"
--- AIPOS/Lab2/infra/k8s.tf
+++ AIPOS/Lab2/infra/k8s.tf
@@ -1,31 +1,41 @@
 resource "kubernetes_namespace" "production" {
-    metadata {
-        name = var.namespace_name
-    }
+  metadata {
+    name = var.namespace_name
+  }
 }
 
 resource "kubernetes_config_map" "database_config" {
   metadata {
-    name      = "database_config"
+    name      = "database-config"
     namespace = kubernetes_namespace.production.metadata[0].name
   }
 
   data = {
-    "host"     = "postgres.postgres"
-    "port"     = "5432"
-    "dbname"   = var.postgres_dbname 
-    "user"     = var.postgres_user
-    "password" = var.postgres_password
+    host     = "postgres.postgres"
+    port     = "5432"
+    dbname   = var.postgres_dbname 
+    user     = var.postgres_user
+    password = var.postgres_password
   }
 }
 
 resource "kubernetes_deployment" "database" {
   metadata {
-    name = "database-deployment"
+    name      = "database-deployment"
+    labels = {
+      app = "database"
+    }
     namespace = kubernetes_namespace.production.metadata[0].name
   }
 
+
   spec {
+    selector {
+      match_labels = {
+        app = "database" 
+      }
+    }
+
     template {
       metadata {
         labels = {
@@ -59,58 +69,70 @@ resource "kubernetes_deployment" "database" {
 }
 
 resource "kubernetes_service" "database" {
-    metadata {
-        name      = "database-service"
-        namespace = kubernetes_namespace.production.metadata[0].name
+  metadata {
+    name      = "database-service"
+    labels = {
+      app = "database"
+    }
+    namespace = kubernetes_namespace.production.metadata[0].name
+  }
+
+  spec {
+    selector = {
+        app = "database"
     }
 
-    spec {
-        selector = {
-            app = "database"
-        }
-
-        port {
-            port        = kubernetes_config_map.database_config.data["port"]
-            target_port = kubernetes_config_map.database_config.data["port"]
-        }
+    port {
+      port        = kubernetes_config_map.database_config.data["port"]
+      target_port = kubernetes_config_map.database_config.data["port"]
     }
+  }
 }
 
 resource "kubernetes_deployment" "app" {
-    metadata {
-        name      = var.deployment_name
-        namespace = kubernetes_namespace.production.metadata[0].name
+  metadata {
+    name      = var.deployment_name
+    labels = {
+      app = "app"
+    }
+    namespace = kubernetes_namespace.production.metadata[0].name
+  }
+
+  spec {
+    replicas = var.replica_count
+
+    selector {
+      match_labels = {
+        app = "app" 
+      }
     }
 
-    spec {
-        replicas = var.replica_count
-
-        selector {
-            match_labels = {
-                app = "App" 
-            }
+    template {
+      metadata {
+        labels = {
+          app = "app" 
         }
+      }
 
-        template {
-            metadata {
-                labels = {
-                    app = "App" 
-                }
-            }
+      spec {
+        container {
+          image = var.container_image
+          name  = var.container_name
 
-            spec {
-                container {
-                    image = var.container_image
-                    name  = var.container_name
-
-                    env {
-                        name      = "DATABASE_URL"
-                        value     = "postgres://${kubernetes_config_map.database_config.data["user"]}:${kubernetes_config_map.database_config.data["password"]}@${kubernetes_config_map.database_config.data["host"]}:${kubernetes_config_map.database_config.data["port"]}/${kubernetes_config_map.database_config.data["dbname"]}"
-                    }
-                }
-            }
+          env {
+            name  = "DATABASE_URL"
+            value = format("postgres://%s:%s@%s:%s/%s",
+              kubernetes_config_map.database_config.data["user"],
+              kubernetes_config_map.database_config.data["password"],
+              kubernetes_config_map.database_config.data["host"],
+              kubernetes_config_map.database_config.data["port"],
+              kubernetes_config_map.database_config.data["dbname"]
+            )
+          }
         }
+      }
     }
+  }
+
+  depends_on = [kubernetes_deployment.database, kubernetes_service.database]
 }
-
-
